95270d58cd3803195abfa9486b9e58323de86b8c
Michael Nganga <ngangamichael83@gmail.com>
2022-07-30 16:32:26 +0300

    changed env to.env, added cors as a liblary then added it to filter in app, configured the routes now it leads to products, created db and table and models

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -35,7 +35,11 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
+
+// was changed to this 
+
+$routes->resource('products');
 
 /*
  * --------------------------------------------------------------------
